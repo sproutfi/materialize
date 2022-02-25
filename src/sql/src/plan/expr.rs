773e309f37ae5355a5c79caac6237b9aed4f9c97
Paul Hemberger <paul@materialize.com>
2022-02-25 10:41:04 -0500

    cleaner enumerate

--- src/sql/src/plan/expr.rs
+++ src/sql/src/plan/expr.rs
@@ -17,7 +17,7 @@ use std::fmt;
 use std::mem;
 
 use anyhow::bail;
-use itertools::{enumerate, Itertools};
+use itertools::Itertools;
 use mz_expr::DummyHumanizer;
 
 use mz_ore::collections::CollectionExt;
@@ -399,7 +399,7 @@ impl AbstractExpr for CoercibleScalarExpr {
             CoercibleScalarExpr::Coerced(expr) => Some(expr.typ(outers, inner, params)),
             CoercibleScalarExpr::LiteralRecord(scalars) => {
                 let mut fields = vec![];
-                for (i, scalar) in enumerate(scalars) {
+                for (i, scalar) in scalars.iter().enumerate() {
                     fields.push((
                         format!("f{}", i + 1).into(),
                         scalar.typ(outers, inner, params)?,
